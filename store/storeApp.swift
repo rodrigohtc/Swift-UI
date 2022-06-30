2157ba4361070daaf20c8b926970c179b7ea379f
Rodrigo Torres <rodrigohtorres@hotmail.com>
2022-06-30 20:40:15 -0300

    Loja Teste

---
+++ store/storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeApp.swift
+//  store
+//
+//  Created by Rodrigo Torres on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
